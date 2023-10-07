3cb8ff03d7837700fc8aec23de8deda0e8881aeb
joelleeyueer <35606477+joelleeyueer@users.noreply.github.com>
2023-10-07 17:26:00 +0800

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+// import Form from './components/Form';
+// import ThankYou from './components/ThankYou';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <div className="App">
+                <Routes>
+                    <Route exact path="/" element={<Login />} />
+                    {/* Add other routes as needed */}
+                </Routes>
+            </div>
+        </Router>
   );
 }
 
