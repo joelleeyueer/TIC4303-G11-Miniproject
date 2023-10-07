0774e9a43d5fc3ca13db3b261f5109bb51a3b3dd
joelleeyueer <35606477+joelleeyueer@users.noreply.github.com>
2023-10-08 00:16:07 +0800

    Added End component

---
+++ client/src/components/End.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function End() {
+    return (
+        <div className="container">
+            <img src="/thank-you.jpg" alt="Thank You" />
+        </div>
+    );
+}
+
+export default End;
