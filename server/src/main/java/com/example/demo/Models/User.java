7dae8ec717bd52531af0e5a24388abc6a1fad445
joelleeyueer <35606477+joelleeyueer@users.noreply.github.com>
2023-10-07 16:19:09 +0800

    Added model

---
+++ server/src/main/java/com/example/demo/Models/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String gender;
+    private String introduction;
+    
+}
